18481ce5db236a662be835da561ba5ba38350566
Oleh Veheria <go.super.0leg@gmail.com>
2020-07-16 12:00:48 +0300

    Initial Commit

---
+++ MoviesDatabaseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesDatabaseApp
+//
+//  Created by Oleh Veheria on 16.07.2020.
+//  Copyright © 2020 Oleh Veheria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
